6804d4286a4a2e18b9c47582ecc406a4ba6e8f7f
quispemerhi350-create <quispemerhi350@gmail.com>
2026-05-26 13:01:10 -0500

    Refactor singleton pattern implementation in C++

--- codigo singlenton.cpp
+++ codigo singlenton.cpp
@@ -1,46 +1,46 @@
-1 #include <iostream>
-2 using namespace std;
-3
-4 // Clase que representa la configuraci n del sistema
-5 class Config {
-6 private:
-7 // nica instancia de la clase (est tica, pertenece a la clase, no a los objetos)
-8 static Config* instancia;
-9
-10 // Constructor PRIVADO: no se puede crear objetos desde fuera
-11 Config() {
-12 cout << "Configuraci n cargada por primera vez.\n";
-13 }
-14
-15 public:
-16 // M todo para obtener la nica instancia
-17 static Config* obtenerInstancia() {
-18 // Si no existe, la creamos
-19 if (!instancia) {
-20 instancia = new Config();
-21 }
-22 // Siempre devolvemos la misma direcci n de memoria
-23 return instancia;
-24 }
-25
-26 // M todo de ejemplo
-27 void mostrarMensaje() {
-28 cout << "Bienvenido al sistema global.\n";
-29 }
-30 };
-31
-32 // Inicializamos el puntero est tico en nulo
-33 Config* Config::instancia = nullptr;
-34
-35 int main() {
-36 // Intentamos crear dos "objetos"
-37 Config* conf1 = Config::obtenerInstancia();
-38 Config* conf2 = Config::obtenerInstancia();
-39
-40 conf1->mostrarMensaje();
-41
-42 // Comprobamos si son el mismo objeto (misma direcci n de memoria)
-43 cout << " Son la misma instancia?-> " << (conf1 == conf2 ? "S " : "NO") << endl;
-44
-45 return 0;
-46 }
+#include <iostream>
+using namespace std;
+
+// Clase que representa la configuraci n del sistema
+class Config {
+private:
+// nica instancia de la clase (est tica, pertenece a la clase, no a los objetos)
+static Config* instancia;
+
+// Constructor PRIVADO: no se puede crear objetos desde fuera
+Config() {
+cout << "Configuraci n cargada por primera vez.\n";
+}
+
+public:
+// M todo para obtener la nica instancia
+static Config* obtenerInstancia() {
+// Si no existe, la creamos
+if (!instancia) {
+instancia = new Config();
+}
+// Siempre devolvemos la misma direcci n de memoria
+return instancia;
+}
+
+// M todo de ejemplo
+void mostrarMensaje() {
+cout << "Bienvenido al sistema global.\n";
+}
+};
+
+// Inicializamos el puntero est tico en nulo
+Config* Config::instancia = nullptr;
+
+int main() {
+// Intentamos crear dos "objetos"
+Config* conf1 = Config::obtenerInstancia();
+Config* conf2 = Config::obtenerInstancia();
+
+conf1->mostrarMensaje();
+
+// Comprobamos si son el mismo objeto (misma direcci n de memoria)
+cout << " Son la misma instancia?-> " << (conf1 == conf2 ? "S " : "NO") << endl;
+
+return 0;
+}
